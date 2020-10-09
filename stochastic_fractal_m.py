4c67522251711e8cc63eeaffdae8cdf3e79af0fc
sha314 <shahnoor3pl@gmail.com>
2020-10-09 18:04:43 +0600

    algorithm by ignoring gaps altogether

--- stochastic_fractal_m.py
+++ stochastic_fractal_m.py
@@ -1,8 +1,8 @@
 from main_py.stochastic_fragmentation import *
-from main_py import df_determination
+# from main_py import df_determination
 from main_py import analytic_solution
 from main_py.analytic_solution import AnalyticSoln
-
+from main_py.stochastic_fragmentation_temp import  *
 FractalLength = Moment
 # find_df = df_determination.find_df
 
